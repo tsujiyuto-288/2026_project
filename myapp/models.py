2449f0663586a8b346083d9b8b0c1473cb07d4ae
itut-vl <camifoure@gmail.com>
2026-02-28 22:07:46 +0900

    ItemProcess.model修正

--- myapp/models.py
+++ myapp/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
-# Create your models here.
-
+# 部品マスターデータ
 class Item(models.Model):
-    item_no = models.CharField(max_length=50)
+    item_no = models.CharField(max_length=50,primary_key=True)
     item_name = models.CharField(max_length=50)
     item_price = models.IntegerField()
     item_deleted = models.BooleanField(default=False)
-    process_list = models.JSONField(blank=True,null=True)#空欄を許可
+    process_list = models.ManyToManyField('Process', through='ItemProcess')
 
+# 部品マスターと工程の中間テーブル
+class ItemProcess(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    process = models.ForeignKey('Process', on_delete=models.CASCADE)
+    process_turn = models.IntegerField()#工程の順番が入る(1とか2とか)
+
+# 受注データ
 class Order(models.Model):
     item_name = models.CharField(max_length=50)
     item_no = models.CharField(max_length=50)
@@ -19,7 +25,7 @@ class Order(models.Model):
     shipping_date = models.CharField(max_length=50)
     shipping_quantity = models.CharField(max_length=50)
 
-
+# 工程データ
 class Process(models.Model):
     process_name = models.CharField(max_length=50)
     process_number = models.IntegerField()
